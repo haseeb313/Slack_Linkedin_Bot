29fff54872f825c95df0413f2aac47f6b07fd976
haseeb313 <87929555+haseeb313@users.noreply.github.com>
2023-07-02 20:52:19 +0500

    testing.py

--- testing/testing.py
+++ testing/testing.py
@@ -7,5 +7,5 @@ channel_name = "#?"
 interval = 60
 
 
-z = Linkedin_Bot(username , password , slack_token , channel_name , interval)
-z.run()
+bot = Linkedin_Bot(username , password , slack_token , channel_name , interval)
+bot.run()
